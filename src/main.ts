b2a99440bbeeff4942eca892a2dd560587965dd4
ecomeel <ecomeel@gmail.com>
2023-12-06 17:08:58 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log('hello')
+let a:string = 'hello';
+
+a = 'sdfsdf';
+
+console.log(a)
+
+
+const greeting = (name:string, age:number):boolean => {
+  console.log('hello', name, age)
+  return true
+}
+
+const b:boolean = greeting('sasha', 23);
+
+console.log(b)
