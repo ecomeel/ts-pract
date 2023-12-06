300338b0f8775ba3afd6e46315083ad05221eb29
ecomeel <ecomeel@gmail.com>
2023-12-06 18:00:54 +0300

    array

--- src/main.ts
+++ src/main.ts
@@ -18,5 +18,14 @@ const newDate = new Date().toDateString();
 const c:Date = new Date();
 console.log(newDate);
 
-const str = 'a b d';
-console.log(str.split(' '))
+const str:string = 'a b d';
+console.log(str.split(' '));
+
+const startString = `shfksd ${str}`;
+console.log(startString)
+
+
+let g:Array<number> = [1, 2, 3];
+
+
+console.log(g)
